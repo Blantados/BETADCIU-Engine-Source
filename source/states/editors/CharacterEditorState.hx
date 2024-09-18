c9d5f8ce8ba79706b03337712ad36b16c7228b37
Blantados <blantados@gmail.com>
2024-09-18 23:17:59 +0800

    json indices check

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -778,7 +778,7 @@ class CharacterEditorState extends MusicBeatState
 			animationLoopCheckBox.checked = anim.loop;
 			animationNameFramerate.value = anim.fps;
 
-			var indicesStr:String = anim.indices.toString();
+			var indicesStr:String = (anim.indices != null ? anim.indices.toString() : "");
 			animationIndicesInputText.text = indicesStr.substr(1, indicesStr.length - 2);
 		});
 		blockPressWhileScrolling.push(animationDropDown);
